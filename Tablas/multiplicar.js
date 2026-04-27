4118fb1490299497cd29dd05fb3dff85ca3351b5
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-27 13:28:05 -0500

    Se genero la tabla del 5

--- Tablas/multiplicar.js
+++ Tablas/multiplicar.js
@@ -1,4 +1,21 @@
 function generarTablas(){
     let contenedor = document.getElementById("contenedor");
-    contenedor.innerHTML = "<h1>PROBANDO</h1>";
+
+    let contenido = "";
+    let numero = 5;
+
+    contenido += "<h1>Tabla del " + numero + "</h1>";
+    contenido += "<table>";
+    contenido += "<tr><th>Operación</th><th>Resultado</th></tr>";
+
+    for(let i = 1; i <= 10; i++){
+        contenido += "<tr>";
+        contenido += "<td>" + numero + " x " + i + "</td>";
+        contenido += "<td>" + (numero * i) + "</td>";
+        contenido += "</tr>";
+    }
+
+    contenido += "</table>";
+
+    contenedor.innerHTML = contenido;
 }
